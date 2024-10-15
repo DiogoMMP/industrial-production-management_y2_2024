7c043a9af85d669f165b67f1cf7a020e3939c19c
tiagosampaio <1230917@isep.ipp.pt>
2024-10-15 17:52:35 +0100

    #34 Made a test for this us

--- src/main/test/java/prodPlanSimulator/repository/HashMapTest.java
+++ src/main/test/java/prodPlanSimulator/repository/HashMapTest.java
@@ -16,12 +16,24 @@ class HashMapTest {
 
     private HashMap_Items_Machines hashMapItemsMachines;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        hashMapItemsMachines = new HashMap_Items_Machines();
+        System.setOut(new PrintStream(outContent));
+        hashMapItemsMachines.addAll();
+    }
 
     @Test
-    void calcOpTime() {
-
-
-
+    void calcOpTime() throws Exception {
+        HashMap<Item, Machine> ProdPlan = new HashMap<>();
+        Item item = new Item();
+        item.setId(10001);
+        Machine machine = new Machine();
+        machine.setId("1");
+        machine.setOperations(null);
+        machine.setTime(10);
+        ProdPlan.put(item, machine);
+        assertEquals(21, hashMapItemsMachines.calcOpTime("cut"));
     }
 }
