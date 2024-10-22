6b59b846672000b70a40c95f71841b9309c3398e
Diogo Pereira 1230948 <1230948@isep.ipp.pt>
2024-10-22 09:08:00 +0100

    #31 I've finished the unit tests for USEI01

--- src/main/test/java/InputFileReaderTest.java
+++ src/main/test/java/InputFileReaderTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import prodPlanSimulator.InputFileReader;
 import prodPlanSimulator.domain.Item;
+import prodPlanSimulator.domain.Workstation;
 import prodPlanSimulator.enums.Priority;
 
 import java.util.List;
@@ -18,25 +19,25 @@ public class InputFileReaderTest {
         Map<Integer, Item> items = InputFileReader.readItems(FILE_PATH_ITEMS);
 
         assertNotNull(items, "The items list should not be null.");
-        assertEquals(15, items.size(), "The number of items read from the file should be 15.");
+        assertEquals(24, items.size(), "The number of items read from the file should be 24.");
 
-        Item item1 = items.get(10001);
-        assertNotNull(item1, "Item 10001 should exist.");
-        assertEquals(Priority.HIGH, item1.getPriority(), "The priority of item 10001 should be HIGH.");
+        Item item1 = items.get(10);
+        assertNotNull(item1, "Item 20 should exist.");
+        assertEquals(Priority.NORMAL, item1.getPriority(), "The priority of item 20 should be NORMAL.");
         List<String> operationsItem1 = item1.getOperations();
-        assertEquals(List.of("CUT", "SAND", "PAINT", "INSPECT"), operationsItem1, "The operations for item 10001 are incorrect.");
+        assertEquals(List.of("CUT", "POLISH", "VARNISH", "PACK"), operationsItem1, "The operations for item 20 are incorrect.");
     }
 
     @Test
     public void testReadMachines() {
-        //Map<String, Machine> machines = InputFileReader.readMachines(FILE_PATH_MACHINES);
+        Map<Integer, Workstation> machines = InputFileReader.readMachines(FILE_PATH_MACHINES);
 
-        //assertNotNull(machines, "The machines list should not be null.");
-        //assertEquals(9, machines.size(), "The number of machines read from the file should be 9.");
+        assertNotNull(machines, "The machines list should not be null.");
+        assertEquals(20, machines.size(), "The number of machines read from the file should be 20.");
 
-        //Machine machine1 = machines.get("ws1");
-        //assertNotNull(machine1, "Machine ws1 should exist.");
-        //assertEquals(List.of("CUT", "PAINT"), machine1.getOperation(), "The operations for machine ws1 are incorrect.");
-        //assertEquals(30, machine1.getTime(), "The total time for machine ws1 is incorrect.");
+        Workstation machine1 = machines.get(10);
+        assertNotNull(machine1, "Workstation ws10 should exist.");
+        assertEquals("INSPECT", machine1.getOperation(), "The operation for machine ws10 are incorrect.");
+        assertEquals(8, machine1.getTime(), "The total time for machine ws10 is incorrect.");
     }
 }
