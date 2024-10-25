55b0e827240539d5452dd46e92c75f6ee67eb376
Diogo Pereira 1230948 <1230948@isep.ipp.pt>
2024-10-25 09:21:02 +0100

    JAR file creation

--- src/main/java/prodPlanSimulator/InputFileReader.java
+++ src/main/java/prodPlanSimulator/InputFileReader.java
@@ -4,55 +4,69 @@ import prodPlanSimulator.domain.Item;
 import prodPlanSimulator.domain.Workstation;
 import prodPlanSimulator.enums.Priority;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.*;
-
 
 public class InputFileReader {
 
-    public static final String FILE_PATH_ITEMS = "src/main/java/prodPlanSimulator/resources/";
-    public static final String FILE_PATH_MACHINES = "src/main/java/prodPlanSimulator/resources/";
 
     public static Map<Integer, Item> readItems(String fileName) throws FileNotFoundException {
         Map<Integer, Item> items = new HashMap<>();
         int increment = 1;
-        Scanner scanner = new Scanner(new File(FILE_PATH_ITEMS + fileName));
-        scanner.nextLine();
 
-        while (scanner.hasNextLine()) {
-            String[] data = scanner.nextLine().split(";");
-            int id = Integer.parseInt(data[0]);
-            String priorityStr = data[1].trim();
-            Priority priority = Priority.fromString(priorityStr);
-            List<String> operations = new ArrayList<>(Arrays.asList(data).subList(2, data.length));
-            Item item = new Item(id, priority, new ArrayList<>());
-            item.getOperations().addAll(operations);
-            items.put(increment, item);
-            increment++;
+
+        InputStream inputStream = InputFileReader.class.getResourceAsStream("/" + fileName);
+        if (inputStream == null) {
+            throw new FileNotFoundException("File not found: " + fileName);
         }
-        scanner.close();
+
+        try (Scanner scanner = new Scanner(inputStream)) {
+            scanner.nextLine();
+
+            while (scanner.hasNextLine()) {
+                String[] data = scanner.nextLine().split(";");
+                int id = Integer.parseInt(data[0]);
+                String priorityStr = data[1].trim();
+                Priority priority = Priority.fromString(priorityStr);
+                List<String> operations = new ArrayList<>(Arrays.asList(data).subList(2, data.length));
+                Item item = new Item(id, priority, new ArrayList<>());
+                item.getOperations().addAll(operations);
+                items.put(increment, item);
+                increment++;
+            }
+        }
+
         return items;
     }
 
     public static Map<Integer, Workstation> readMachines(String fileName) throws FileNotFoundException {
         Map<Integer, Workstation> machines = new HashMap<>();
         int increment = 1;
-        Scanner scanner = new Scanner(new File(FILE_PATH_MACHINES + fileName));
-        scanner.nextLine();
 
-        while (scanner.hasNextLine()) {
-            String[] data = scanner.nextLine().split(";");
-            String id = data[0];
-            String operation = data[1];
-            int time = Integer.parseInt(data[2]);
-            Workstation workstation = new Workstation();
-            workstation.setId(id);
-            workstation.setOperation(operation);
-            workstation.setTime(time);
-            machines.put(increment, workstation);
-            increment++;
+
+        InputStream inputStream = InputFileReader.class.getResourceAsStream("/" + fileName);
+        if (inputStream == null) {
+            throw new FileNotFoundException("File not found: " + fileName);
         }
-        scanner.close();
+
+        try (Scanner scanner = new Scanner(inputStream)) {
+            scanner.nextLine();
+
+            while (scanner.hasNextLine()) {
+                String[] data = scanner.nextLine().split(";");
+                String id = data[0];
+                String operation = data[1];
+                int time = Integer.parseInt(data[2]);
+                Workstation workstation = new Workstation();
+                workstation.setId(id);
+                workstation.setOperation(operation);
+                workstation.setTime(time);
+                machines.put(increment, workstation);
+                increment++;
+            }
+        }
+
         return machines;
     }
 }
--- src/main/java/projectManager/ProductStructureGraph.java
+++ src/main/java/projectManager/ProductStructureGraph.java
@@ -1,8 +1,9 @@
 package projectManager;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
@@ -14,24 +15,28 @@ import guru.nidi.graphviz.model.MutableNode;
 
 import static guru.nidi.graphviz.model.Factory.*;
 
-import guru.nidi.graphviz.engine.GraphvizV8Engine;
-
 public class ProductStructureGraph {
 
-    private static final String INPUT_FILE_PATH = "src/main/java/projectManager/input/";
     private static final String OUTPUT_FILE_PATH = "src/main/java/projectManager/output/product_structure.svg";
 
     public void generateGraph(String csvFileName) {
         String line;
         String csvSplitBy = ";";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE_PATH + csvFileName))) {
+        try (InputStream inputStream = getClass().getResourceAsStream("/" + csvFileName);
+             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+
             MutableGraph graph = mutGraph("Product Structure").setDirected(true);
 
             br.readLine();
 
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(csvSplitBy);
+
+                if (data.length < 4) {
+                    System.err.println("Insufficient data on the line: " + line);
+                    continue;
+                }
 
                 String productId = data[0];
                 String partNumber = data[1];
@@ -49,8 +54,8 @@ public class ProductStructureGraph {
 
             System.out.println("Graph successfully generated in: " + outputPath);
         } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
             e.printStackTrace();
         }
     }
 }
-
