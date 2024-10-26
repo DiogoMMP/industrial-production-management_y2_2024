8c819dbe8eff38baa3aa037927081afa97fa0a11
HP <1230462@isep.ipp.pt>
2024-10-26 18:03:05 +0100

    Possible error in ui

--- src/main/java/prodPlanSimulator/UI/Menu/MainMenuUI.java
+++ src/main/java/prodPlanSimulator/UI/Menu/MainMenuUI.java
@@ -15,22 +15,27 @@ public class MainMenuUI implements Runnable {
      */
     @Override
     public void run() {
-        List<MenuItem> options = new ArrayList<>();
-        options.add(new MenuItem("Simulate Process", new SimulateProcessUI()));
-        options.add(new MenuItem("Total Time", new TotalTimeUI()));
-        options.add(new MenuItem("Time of Workstation", new WorkstationTimeUI()));
-        options.add(new MenuItem("Execution Time", new ListExecutionTimeUI()));
-        options.add(new MenuItem("Average and waiting times", new AverageAndWaitingTimesUI()));
-        options.add(new MenuItem("Flow Dependency", new FlowDependencyUI()));
-        options.add(new MenuItem("Product Structure Graph", new ProductStructureGraphUI()));
-        int option = 0;
-        do {
-            option = Utils.showAndSelectIndex(options, "\n\n--- MAIN MENU --------------------------");
+        try {
+            List<MenuItem> options = new ArrayList<>();
+            options.add(new MenuItem("Simulate Process", new SimulateProcessUI()));
+            options.add(new MenuItem("Total Time", new TotalTimeUI()));
+            options.add(new MenuItem("Time of Workstation", new WorkstationTimeUI()));
+            options.add(new MenuItem("Execution Time", new ListExecutionTimeUI()));
+            options.add(new MenuItem("Average and waiting times", new AverageAndWaitingTimesUI()));
+            options.add(new MenuItem("Flow Dependency", new FlowDependencyUI()));
+            options.add(new MenuItem("Product Structure Graph", new ProductStructureGraphUI()));
+            int option = 0;
+            do {
+                option = Utils.showAndSelectIndex(options, "\n\n--- MAIN MENU --------------------------");
 
-            if ((option >= 0) && (option < options.size())) {
-                options.get(option).run();
-            }
-        } while (option != -1);
+                if ((option >= 0) && (option < options.size())) {
+                    options.get(option).run();
+                }
+            } while (option != -1);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 }
 
--- src/main/java/prodPlanSimulator/UI/Utils/DataInitializer.java
+++ src/main/java/prodPlanSimulator/UI/Utils/DataInitializer.java
@@ -23,33 +23,38 @@ public class DataInitializer implements Runnable {
      */
     @Override
     public void run() {
-        Scanner scanner = new Scanner(System.in);
-        String pathArt;
-        String pathWor;
-        boolean success = false;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            String pathArt;
+            String pathWor;
+            boolean success = false;
 
-        while (!success) {
-            System.out.println("\n\n--- DATA --------------------------");
-            System.out.println("1. Use default file paths");
-            System.out.println("2. Enter file paths manually");
+            while (!success) {
+                System.out.println("\n\n--- DATA --------------------------");
+                System.out.println("1. Use default file paths");
+                System.out.println("2. Enter file paths manually");
 
-            System.out.print("\n\nType your option: ");
-            int choice = Integer.parseInt(scanner.nextLine());
+                System.out.print("\n\nType your option: ");
+                int choice = Integer.parseInt(scanner.nextLine());
 
-            if (choice == 1) {
-                pathArt = "articles.csv";
-                pathWor = "workstations.csv";
-            } else {
-                pathArt = Utils.readLineFromConsole("Articles: ");
-                pathWor = Utils.readLineFromConsole("Workstations: ");
+                if (choice == 1) {
+                    pathArt = "articles.csv";
+                    pathWor = "workstations.csv";
+                } else {
+                    pathArt = Utils.readLineFromConsole("Articles: ");
+                    pathWor = Utils.readLineFromConsole("Workstations: ");
+                }
+
+                try {
+                    init(pathArt, pathWor);
+                    success = true;
+                } catch (FileNotFoundException e) {
+                    System.err.println("Error: File not found. Please check the file path and try again.");
+                }
             }
-
-            try {
-                init(pathArt, pathWor);
-                success = true;
-            } catch (FileNotFoundException e) {
-                System.err.println("Error: File not found. Please check the file path and try again.");
-            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
+
     }
 }
