925b837c355b6e732f263e59e9faf02acd4b4085
HP <1230462@isep.ipp.pt>
2024-11-19 12:29:24 +0000

    #65 US03 output in the main now is how the client wants.
    Time 0h10

--- machineSupervisor/USAC03/main.c
+++ machineSupervisor/USAC03/main.c
@@ -7,36 +7,65 @@ int main() {
     // Test case 1: Valid input
     char str1[] = " 89 ";
     int res = get_number(str1, &value);
-    printf("%d: %d\n", res, value); // Expected: 1: 89
+    if (res == 1) {
+        printf("%d: %d\n", res, value); // Expected: 1: 89
+    } else {
+        printf("%d: \n", res); // Expected: 0: 
+    }
 
     // Test case 2: Invalid input
     char str2[] = " 8 - -9 ";
     res = get_number(str2, &value);
-    printf("%d: %d\n", res, value); // Expected: 0: -1 (Invalid input)
+    if (res == 1) {
+        printf("%d: %d\n", res, value); 
+    } else {
+        printf("%d: \n", res); // Expected: 0: 
+    }
 
     // Test case 3: Negative number
     char str3[] = "-1234";
     res = get_number(str3, &value);
-    printf("%d: %d\n", res, value); // Expected: 0: -1 (Invalid input)
+    if (res == 1) {
+        printf("%d: %d\n", res, value); 
+    } else {
+        printf("%d: \n", res); // Expected: 0: 
+    }
 
     // Test case 4: Leading spaces and tabs
     char str4[] = "   \t   5678";
     res = get_number(str4, &value);
-    printf("%d: %d\n", res, value); // Expected: 1: 5678
+    if (res == 1) {
+        printf("%d: %d\n", res, value); // Expected: 1: 5678
+    } else {
+        printf("%d: \n", res); // Expected: 0: 
+    }
 
     // Test case 5: Empty string
     char str5[] = "";
     res = get_number(str5, &value);
-    printf("%d: %d\n", res, value); // Expected: 0: -1 (Invalid input)
+    if (res == 1) {
+        printf("%d: %d\n", res, value); 
+    } else {
+        printf("%d: \n", res); // Expected: 0: 
+    }
 
     // Test case 6: Null string
     char str6[] = "\0";
     res = get_number(str6, &value);
-    printf("%d: %d\n", res, value); // Expected: 0: -1 (Invalid input)
+    if (res == 1) {
+        printf("%d: %d\n", res, value); 
+    } else {
+        printf("%d: \n", res); // Expected: 0: 
+    }
 
     // Test case 7: Signal '+' before number
     char str7[] = "+1234";
     res = get_number(str7, &value);
-    printf("%d: %d\n", res, value); // Expected: 0: -1 (Invalid input)
+    if (res == 1) {
+        printf("%d: %d\n", res, value); 
+    } else {
+        printf("%d: \n", res); // Expected: 0: 
+    }
+
     return 0;
 }
