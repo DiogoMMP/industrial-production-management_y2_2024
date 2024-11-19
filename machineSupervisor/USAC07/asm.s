c8b3d32f364126c2311907161622e64b206b919e
tiagosampaio <1230917@isep.ipp.pt>
2024-11-19 10:02:03 +0000

    #69 Made a small correction on my USAC07 to pass al the tests

--- machineSupervisor/USAC07/asm.s
+++ machineSupervisor/USAC07/asm.s
@@ -1,7 +1,7 @@
 .section .text
-    .global get_n_elements
+    .global get_n_element
 
-get_n_elements:
+get_n_element:
     # %rdi = buffer (not used here)
     # %esi = num (size of the buffer)
     # %rdx = tail (pointer to tail)
@@ -13,14 +13,12 @@ get_n_elements:
 
     # Load the values of tail and head
     movl (%rdx), %eax             # Load the value of tail into %eax
-    movl (%rcx), %ebx             # Load the value of head into %ebx
+    movl (%rcx), %r8d             # Load the value of head into %ebx
 
-    subl $1, %esi                 # Decrement num by 1 (effective buffer size is num-1)
-
-    cmpl %eax, %ebx               # Compare head (ebx) and tail (eax)
+    cmpl %eax, %r8d               # Compare head (ebx) and tail (eax)
     je buffer_empty               # If head == tail, the buffer is empty
 
-    movl %ebx, %ecx               # Copy head to ecx
+    movl %r8d, %ecx               # Copy head to ecx
     addl $1, %ecx                 # Increment head by 1 (head + 1)
     cmpl %esi, %ecx               # Check if head + 1 exceeds the buffer size
     jne not_wrapped               # If not, continue
@@ -31,18 +29,18 @@ not_wrapped:
     je buffer_full                # If they are equal, the buffer is full
 
     # Buffer has elements
-    cmpl %eax, %ebx               # Check if head >= tail
+    cmpl %eax, %r8d               # Check if head >= tail
     jge head_greater_or_equal_tail
 
     # Case: tail > head (circular wrap-around)
     movl %esi, %ecx               # ecx = num (size of buffer - 1)
     subl %eax, %ecx               # ecx = num - tail
-    addl %ebx, %ecx               # ecx = num - tail + head
+    addl %r8d, %ecx               # ecx = num - tail + head
     jmp end
 
 head_greater_or_equal_tail:
-    subl %eax, %ebx               # ebx = head - tail
-    movl %ebx, %ecx               # Store result in ecx
+    subl %eax, %r8d               # ebx = head - tail
+    movl %r8d, %ecx               # Store result in ecx
 
     jmp end
 
@@ -51,7 +49,7 @@ buffer_empty:
     jmp epilogue
 
 buffer_full:
-    movl %esi, %eax               # Buffer is full, return num - 1
+    movl %esi, %eax               # Buffer is full
     jmp epilogue
 
 end:
--- machineSupervisor/USAC07/main.c
+++ machineSupervisor/USAC07/main.c
@@ -11,25 +11,25 @@ int main() {
     // Test 1: Buffer is empty (tail == head)
     tail = 0;
     head = 0;
-    result = get_n_elements(buffer, num, &tail, &head);
+    result = get_n_element(buffer, num, &tail, &head);
     printf("Test 1 (Empty buffer): Expected: 0, Result: %d\n", result);
 
     // Test 2: Buffer is full (head one position behind tail in a circular buffer)
     tail = 0;
     head = 9;  // Full buffer example in circular logic
-    result = get_n_elements(buffer, num, &tail, &head);
+    result = get_n_element(buffer, num, &tail, &head);
     printf("Test 2 (Full buffer): Expected: %d, Result: %d\n", num-1, result);
 
     // Test 3: Buffer has some elements (head > tail)
     tail = 2;
     head = 6;
-    result = get_n_elements(buffer, num, &tail, &head);
+    result = get_n_element(buffer, num, &tail, &head);
     printf("Test 3 (Partially filled buffer, head > tail): Expected: 4, Result: %d\n", result);
 
     // Test 4: Circular buffer (tail > head)
     tail = 8;
     head = 3;  // In a circular buffer, this should calculate the wrap-around difference
-    result = get_n_elements(buffer, num, &tail, &head);
+    result = get_n_element(buffer, num, &tail, &head);
     printf("Test 4 (Circular buffer): Expected: 5, Result: %d\n", result);
 
     return 0;
