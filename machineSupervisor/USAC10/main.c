2c960a4cf944676275f6b3b9e54a72129e06f168
tiagosampaio <1230917@isep.ipp.pt>
2024-11-20 14:55:17 +0000

    #72 Fixed the function for USAC10 and finished the US

--- machineSupervisor/USAC10/main.c
+++ machineSupervisor/USAC10/main.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
 #include "asm.h"
+#include "sort_array.h"
 
 int main(){
     // Define the array to be tested
-    int array[] = {5, 3, 9, 1, 6, 2};
+    int array[] = {1, 3, 2, 4, 5, 6};
+    int array2[] = {0,3000,10,20,0,300};
 
     // Define variables
     int median_value;
+    int * ptr_median_value = &median_value;
     int length = sizeof(array) / sizeof(array[0]);  // Calculate length of the array
+    int length2 = sizeof(array2) / sizeof(array2[0]);  // Calculate length of the array
+
+
 
     // Call the median function
-    int result = median(array, length, &median_value);
-
-    // Check if the median function succeeded
+    int result = median(array, length, ptr_median_value);
     if (result == 1) {
-        printf("Median: %d\n", median_value);  // Print the median value
+        printf("Median: %d\n", *ptr_median_value);  // Print the median value
     } else {
         printf("Error: Invalid array length\n");
     }
+
+        // Print the sorted arrays
+    printf("Sorted array: ");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    int result2 = median(array2, length2, ptr_median_value);
+
+        if (result2 == 1) {
+        printf("Median: %d\n", *ptr_median_value);  // Print the median value
+    } else {
+        printf("Error: Invalid array length\n");
+    }
+
+        // Print the sorted arrays
+    printf("Sorted array: ");
+    for (int i = 0; i < length2; i++) {
+        printf("%d ", array2[i]);
+    }
+    printf("\n");
+
     return 0;
+
 }
