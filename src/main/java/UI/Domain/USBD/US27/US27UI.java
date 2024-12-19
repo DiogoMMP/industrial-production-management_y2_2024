a0ca511bf7e8ec0258c0f3a4392b66efacd625d9
Diogo Pereira 1230948 <1230948@isep.ipp.pt>
2024-12-19 14:59:56 +0000

    USBD27 UI Time: 20min

--- src/main/java/UI/Domain/USBD/US27/US27UI.java
+++ src/main/java/UI/Domain/USBD/US27/US27UI.java
@@ -1,6 +1,7 @@
 package UI.Domain.USBD.US27;
 
 import UI.Menu.MenuItem;
+import UI.Menu.Sprint3MenuUI;
 import UI.Utils.Utils;
 import importer_and_exporter.OracleDataExporter;
 
@@ -26,12 +27,11 @@ public class US27UI implements Runnable {
 
                 int option;
                 do {
-                    option = Utils.showAndSelectIndex(options,
-                            "\n\n" + "--- Choose the Customer Order to Reserve Materials ------------\n");
+                    option = Utils.showAndSelectIndex(options, "\n\n" + Utils.BOLD + Utils.CYAN +
+                            "--- Choose the Customer Order to Reserve Materials ------------\n" + Utils.RESET);
 
                     if (option == -2) {
-                        System.out.println("Returning to the previous menu...");
-                        return;
+                        new Sprint3MenuUI().run();
                     }
 
                     if ((option >= 0) && (option < options.size())) {
@@ -64,14 +64,23 @@ public class US27UI implements Runnable {
 
     private void reserveMaterialsForOrder(int customerOrderId) throws SQLException {
         String call = "{ ? = call reserve_materials_for_order(?) }";
+        String result;
 
         try (Connection connection = getConnection();
              CallableStatement callableStatement = connection.prepareCall(call)) {
 
             callableStatement.registerOutParameter(1, Types.VARCHAR);
             callableStatement.setInt(2, customerOrderId);
+
             callableStatement.execute();
+
             callableStatement.getString(1);
+
+            System.out.println(Utils.GREEN + "\nMaterials reserved successfully for Customer Order ID: " + customerOrderId + Utils.RESET);
+
+        } catch (SQLException e) {
+            System.out.println(Utils.RED + "\nFailed to reserve materials for Customer Order ID: " + customerOrderId +
+                    " - " + e.getMessage() + Utils.RESET);
         }
     }
 
