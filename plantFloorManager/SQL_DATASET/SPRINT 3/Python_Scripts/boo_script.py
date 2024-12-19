a4645182f7024cf8547af95e71253556251d053a
Diogo Pereira 1230948 <1230948@isep.ipp.pt>
2024-12-19 10:42:58 +0000

    Correction of the Python scripts for the new data and generation of the inserts. I also corrected the relational model due to the Expected Estimated Time. Time: 45min

--- plantFloorManager/SQL_DATASET/SPRINT 3/Python_Scripts/boo_script.py
+++ plantFloorManager/SQL_DATASET/SPRINT 3/Python_Scripts/boo_script.py
@@ -2,7 +2,7 @@ import random
 import xml.etree.ElementTree as ET
 
 
-def generate_inserts(xml_file, table_name1, table_name2, table_name3):
+def generate_inserts(xml_file, table_name1, table_name2, table_name3, table_name4):
     # Parse the XML file
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -11,6 +11,7 @@ def generate_inserts(xml_file, table_name1, table_name2, table_name3):
     inserts_boo = []
     inserts_boo_input = []
     inserts_boo_output = []
+    inserts_operations = []
     enter = ['\n']
 
     # Iterate over 'boo' elements in the XML
@@ -22,19 +23,27 @@ def generate_inserts(xml_file, table_name1, table_name2, table_name3):
         for operation in boo.findall('.//operation'):
 
             operation_id = operation.get('id')
+            operation_type_id = operation.find('optype_id').text.strip('"')
+            eet = operation.find('eet').text.strip('"')
             next_operation_id = operation.find('next_op').text.strip('"')
+
+            insert_operation = (
+                f"INSERT INTO {table_name4} (Operation_ID, Operation_Type_ID, Expected_Estimated_Time) "
+                f"VALUES ({operation_id}, {operation_type_id}, {eet});"
+            )
+            inserts_operations.append(insert_operation)
 
             if next_operation_id == 'None':
                 insert_boo = (
                     f"INSERT INTO {table_name1} (Operation_ID, Product_ID) "
-                    f" VALUES ({operation_id}, '{product_id}');"
+                    f"VALUES ({operation_id}, '{product_id}');"
                 )
                 inserts_boo.append(insert_boo)
 
             else:
                 insert_boo = (
                     f"INSERT INTO {table_name1} (Operation_ID, Product_ID, Next_Operation_ID) "
-                    f" VALUES ({operation_id}, '{product_id}', {next_operation_id});"
+                    f"VALUES ({operation_id}, '{product_id}', {next_operation_id});"
                 )
                 inserts_boo.append(insert_boo)
 
@@ -45,7 +54,7 @@ def generate_inserts(xml_file, table_name1, table_name2, table_name3):
 
                 insert_input = (
                     f"INSERT INTO {table_name2} (Product_ID, Operation_ID, Part_ID, Quantity, Unit) "
-                    f" VALUES ('{product_id}', {operation_id}, '{part_id}', {quantity}, '{unit}');"
+                    f"VALUES ('{product_id}', {operation_id}, '{part_id}', {quantity}, '{unit}');"
                 )
                 inserts_boo_input.append(insert_input)
 
@@ -56,20 +65,21 @@ def generate_inserts(xml_file, table_name1, table_name2, table_name3):
 
                 insert_output = (
                     f"INSERT INTO {table_name3} (Product_ID, Operation_ID, Part_ID, Quantity, Unit) "
-                    f" VALUES ('{product_id}', {operation_id}, '{part_id}', {quantity}, '{unit}');"
+                    f"VALUES ('{product_id}', {operation_id}, '{part_id}', {quantity}, '{unit}');"
                 )
                 inserts_boo_output.append(insert_output)
 
-    return inserts_boo + enter + inserts_boo_input + enter + inserts_boo_output
+    return inserts_operations + enter + inserts_boo + enter + inserts_boo_input + enter + inserts_boo_output
 
 
 # XML file and table names
-xml_file_path = 'Dataset_S3_boo_V01.xml'
+xml_file_path = 'Dataset_S3_boo_V02.xml'
 table_name1 = 'BOO'
 table_name2 = 'BOO_Input'
 table_name3 = 'BOO_Output'
+table_name4 = 'Operation'
 
 # Generate INSERT commands
-insert_statements = generate_inserts(xml_file_path, table_name1, table_name2, table_name3)
+insert_statements = generate_inserts(xml_file_path, table_name1, table_name2, table_name3, table_name4)
 for statement in insert_statements:
     print(statement)
